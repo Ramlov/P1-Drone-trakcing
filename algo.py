90e3f948cbd31a91c3c44e54a7c8d0a3cf99df22
Ramlov <mathias.ramlov10@gmail.com>
2022-10-25 21:24:20 +0200

    Smuk gui og noise :D

--- algo.py
+++ algo.py
@@ -1,8 +1,12 @@
 from random import random
 import numpy as np
 import math
-import matplotlib.pyplot as plt
 import random
+import matplotlib 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import PySimpleGUI as sg
+matplotlib.use("TkAgg")
 
 
 class graph():
@@ -12,6 +16,11 @@ class graph():
         self.xpos = xpos
         self.ypos = ypos
 
+    def reset(self):
+        xpos = []
+        ypos = []
+        self.xpos = xpos
+        self.ypos = ypos
 
     def movement(self, xs, ys, vx, vy, t_end):
         #Define Lists
@@ -20,7 +29,6 @@ class graph():
             change.append((t_end/4)*i)
 
 
-        
         #X and y Acceleration and velocity
         ax = 0
         ay = 0
@@ -55,46 +63,71 @@ class graph():
                 ay = (random.randrange(-15, 15, 1) / 10)
                 print(ax, ay)
         #plt.plot(self.xpos, self.ypos)
-        #plt.show()
 
     def show_graph(self):
         '''Generate Graph'''
         #plt.show()
 
-    def noise(self):
+    def noise(self, spread, howmuch):
         '''Generate Noise'''
         random.seed(42)
-        N = len(self.xpos)
-        rang = range(N)
-        new_xpos = [random.random() for i in rang]
-
-        B = len(self.ypos)
-        rangg = range(B)
-        new_ypos = [random.random() for I in rangg]
-        #print(new_xpos, new_ypos)
-        for l in range(B):
-            test = []
-            scnd = random.random()
-            yikes = self.ypos+scnd
-            test.append(yikes)
+        test = []
+        test1 = []
+        for m in range(howmuch):
+            for l in range(len(self.xpos)):
+                test.append(self.xpos[l-1]+(random.random()*spread)) # Random seed
+            
+            for k in range(len(self.ypos)):
+                test1.append(self.ypos[k-1]+(random.random()*spread)) #Random seed
         
-        for k in range(N):
-            test1 = []
-            test1.append(self.xpos+random.random())
-
-
-        fig = plt.figure()
         fig, ax = plt.subplots()
         ax.plot(self.xpos, self.ypos, label='Movement')
         ax.plot(test, test1, 'r+', label='Noise')
-        ax.set_title("My Plot")
+        ax.set_title("Drone movement + Noise")
         ax.legend()
-        #plt.plot(new_xpos, new_ypos, 'r+')
-        #plt.plot(self.xpos, self.ypos)
-        plt.show()
+        return(fig)
+        #plt.show()
 
 graf = graph()
 
-graf.movement(0, 0, 0, 0, 100)
-graf.noise()
+#graf.movement(0, 0, 0, 0, 100)
+#graf.noise()
 #plt.show()
+
+
+
+def draw_figure(canvas, figure): #Figure on canvas
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
+    return figure_canvas_agg
+
+def delete_fig(fig): #Delete or update figure
+    fig.get_tk_widget().forget()
+    plt.close('all')
+
+sg.theme('DarkGrey4')  
+layout = [
+            [sg.Button('Lav graf',  key='-MAKE-')],
+            [sg.Text('Hvor stor afspredelse?'), sg.Input('15', key='-SPREAD-')],
+            [sg.Text('Hvor meget støj'),sg.Input('1', key='-HOWMUCH-')],
+            [sg.Canvas(key = '-graph-')],
+        ]
+
+window = sg.Window('Drone Tracking Algorithm', layout, grab_anywhere=True)
+
+fig_gui = None
+
+
+while True:
+    print(fig_gui)
+    event, values = window.read()
+    if event == None or event == 'Exit':
+        break
+    if event == '-MAKE-':
+        if fig_gui != None:
+            delete_fig(fig_gui)
+        graf.reset()
+        graf.movement(0, 0, 0, 0, 100)
+        fig = graf.noise(int(values['-SPREAD-']), int(values['-HOWMUCH-']))
+        fig_gui = draw_figure(window['-graph-'].TKCanvas, fig)
