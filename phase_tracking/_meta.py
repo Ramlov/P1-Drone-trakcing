46fa7aa029acadd1f97a28a1f8fc16fcddcc59c2
Ramlov <mathias.ramlov10@gmail.com>
2022-11-09 09:52:10 +0100

    Phase tracking

---
+++ phase_tracking/_meta.py
@@ -0,0 +1,17 @@
+from acconeer.exptool.a111.algo import ModuleFamily, ModuleInfo
+
+from ._processor import Processor, get_sensor_config
+from .ui import PGUpdater
+
+
+module_info = ModuleInfo(
+    key="iq_phase_tracking",
+    label="Phase tracking (IQ)",
+    pg_updater=PGUpdater,
+    processing_config_class=lambda: None,
+    module_family=ModuleFamily.EXAMPLE,
+    sensor_config_class=get_sensor_config,
+    processor=Processor,
+    multi_sensor=False,
+    docs_url="https://docs.acconeer.com/en/latest/processing/phase_tracking.html",
+)
