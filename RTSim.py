721edbe62712a1916181b77a9db28d13ff3d078b
Ramlov <mathias.ramlov10@gmail.com>
2022-11-15 11:13:51 +0100

    Create RTSim.py

---
+++ RTSim.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2022
+#  ___            _         
+# | _ \__ _ _ __ | |_____ __
+# |   / _` | '  \| / _ \ V /
+# |_|_\__,_|_|_|_|_\___/\_/ 
+#   ___ _  _ ___ ___ ___ _____ ___   _   _  _ __  __ ___  __   __   __  
+#  / __| || | _ \_ _/ __|_   _|_ _| /_\ | \| |  \/  / _ \/  \ /  \ /  \ 
+# | (__| __ |   /| |\__ \ | |  | | / _ \| .` | |\/| \_, / () | () | () |
+#  \___|_||_|_|_\___|___/ |_| |___/_/ \_\_|\_|_|  |_|/_/ \__/ \__/ \__/ 
+
+
+import acconeer.exptool as et
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    #Initialize Radar and Session
+    args = et.a111.ExampleArgumentParser().parse_args()
+    et.utils.config_logging(args)
+
+    client = et.a111.Client(**et.a111.get_client_args(args))
+
+    client.squeeze = False
+
+    sensor_config = et.a111.EnvelopeServiceConfig()
+    sensor_config.sensor = args.sensors
+    sensor_config.range_interval = [0.2, 5.0]
+    sensor_config.profile = sensor_config.Profile.PROFILE_2
+    sensor_config.hw_accelerated_average_samples = 5
+    sensor_config.downsampling_factor = 2
+
+    session_info = client.setup_session(sensor_config)
+    print("Session info:\n", session_info, "\n")
+
+    #Figure and Plot Creation
+    fig, (amplitude_ax) = plt.subplots(1)
+    line, = amplitude_ax.plot([0, 100], sensor_config.range_interval)
+
+    plt.title("1 Dimensional Drone Flight", fontsize=20)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Distance (cm)")
+
+    #Turn on Auto Plot
+    plt.ion()
+
+    #Draw Figure to window
+    #fig_gui = draw_figure(window['-graph-'].TKCanvas, fig)
+
+    #First Time Variable Creation
+    iter = 0 #iterations
+    frq = 0.2 #Frequency If we use time
+    length = [] #Estimated Length to Drone
+    tlist = [] #Time History
+
+    #Session Start
+    client.start_session()
+
+    interrupt_handler = et.utils.ExampleInterruptHandler()
+    print("Press Ctrl-C to end session\n")
+
+
+    depths = et.a111.get_range_depths(sensor_config, session_info)
+
+    while not interrupt_handler.got_signal:
+        #Data Collection
+        info, data = client.get_next()
+        index = np.argmax(data)
+        dist = depths[index] #Det er din data Christi <3
+
+        #Data Storing And Plotting
+        amplitude_ax.plot(iter, dist, 'r+') #Plot time and data point
+        fig.canvas.draw() #Draw Data
+        fig.canvas.flush_events() #Wait until Drawn
+        tlist.append(iter)
+        length.append(dist)
+        iter += 1 #Update Time/Iteration interval
+
+    print("Disconnecting...")
+    client.disconnect()
+
+    #When Done Collecting Data
+    amplitude_ax.clear()
+    amplitude_ax.plot(tlist, length, 'r+', label='Noise', linewidth=0.2)
+    amplitude_ax.set_title("Drone movement + Noise")
+    fig.canvas.draw()
+
+    #Turn off Autodisplay After Data Collection and Plotting
+    plt.ioff()
+
+if __name__ == "__main__":
+    main()
